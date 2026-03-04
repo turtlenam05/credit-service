2e13dc938d044b577ebb8e14128fe509072ba22a
DatHoQuoc <dathoquocdn23@gmail.com>
2026-03-04 14:06:07 +0700

    add CORS

---
+++ src/main/java/com/dathq/swd302/creditservice/config/BeansConfig.java
@@ -0,0 +1,40 @@
+package com.dathq.swd302.creditservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class BeansConfig {
+    @Bean
+    public CorsFilter corsFilter(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("https://estate.maik.io.vn",
+                "http://localhost:5173"));
+        config.setAllowedHeaders(Arrays.asList(
+                        HttpHeaders.ORIGIN,
+                        HttpHeaders.CONTENT_TYPE,
+                        HttpHeaders.ACCEPT,
+                        HttpHeaders.AUTHORIZATION,
+                        "x-user-id"
+                )
+        );
+        config.setAllowedMethods(Arrays.asList(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "PATCH"
+        ));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
