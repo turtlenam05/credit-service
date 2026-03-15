cfe107c2e9fa9b945a0a899452d66aa96ae44934
Maik <maikk19funi@gmail.com>
2026-03-16 00:02:05 +0700

    fix: unify route naming

--- src/main/java/com/dathq/swd302/creditservice/controller/CreditSettingController.java
+++ src/main/java/com/dathq/swd302/creditservice/controller/CreditSettingController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/admin/credit-settings")
+@RequestMapping("/api/v1/admin/credit-settings")
 @RequiredArgsConstructor
 @Slf4j
 public class CreditSettingController {
